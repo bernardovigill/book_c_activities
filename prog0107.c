daa7fc12e602b11679d86d6014ccedf0d49f8b7f
Bernardo Alves Vigil <155205432+bernardovigill@users.noreply.github.com>
2025-09-04 20:48:08 -0300

    prog0107.c

---
+++ prog0107.c
@@ -0,0 +1,11 @@
+/*Escreva um programa em C que apresente duas linhas com a string "Aqui vai um Apito",
+ouvindo-se ao final de cada string um sinal sonoro.*/
+
+#include <stdio.h>
+
+main()
+{
+    printf("Aqui vai um Apito\7\n\n"); /* \7 representa o sinal sonoro*/
+    printf("Aqui vai um Apito\7\n");
+    
+}
