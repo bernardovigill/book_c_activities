80b8ad4de7d7a2bfd6d4f146f74399e9087828f0
Bernardo Alves Vigil <155205432+bernardovigill@users.noreply.github.com>
2025-09-04 20:26:26 -0300

    Create prog0106.c

---
+++ prog0106.c
@@ -0,0 +1,13 @@
+/*Escreva um programa em C que apresente a seguinte saída:*/
+#include <stdio.h>
+
+int main()
+{
+    printf("1-      Clientes\n");
+    printf("2-      Fornecedores\n");
+    printf("3-      Faturas\n\n");
+    printf("0-      Sair\n");
+    
+
+    return 0;
+}  
