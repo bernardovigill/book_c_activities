74d6c792c2c2d85719dcc14f5cb07a273be54d5c
Bernardo Alves Vigil <155205432+bernardovigill@users.noreply.github.com>
2025-09-05 00:29:42 -0300

    prog0109.c

---
+++ prog0109.c
@@ -0,0 +1,10 @@
+/* Escreva um programa que coloque na tela a seguinte frase:
+* Bem-vindos ao /Mundo\ da programação em "C"
+*/
+
+#include <stdio.h>
+
+main ()
+{
+    printf("Bem-vindos ao /Mundo\\ da programação em \"C\"");
+}
