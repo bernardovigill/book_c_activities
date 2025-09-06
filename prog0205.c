e24fca861b5224633b1d63e6471a0bc1467b3fdb
Bernardo Alves Vigil <155205432+bernardovigill@users.noreply.github.com>
2025-09-06 17:56:46 -0300

    prog0205.c

---
+++ prog0205.c
@@ -0,0 +1,14 @@
+/* Escreva um programa que indique qual o nº de bytes que ocupan todos os tipo de dados básicos em C*/
+
+#include <stdio.h>
+
+main ()
+
+{
+    printf("O tamanho em bytes de um inteiro = %d\n", sizeof(int));
+    printf("O tamanho em bytes de um char = %d\n", sizeof(char));
+    printf("O tamanho em bytes de um float = %d\n", sizeof(float));
+    printf("O tamanho em bytes de um double = %d\n", sizeof(double));
+    
+}
+ 
